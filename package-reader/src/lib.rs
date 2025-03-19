aaf7bb4dd478d5165d73d64a214d2ff633c15417
cloud-np <cloud-np@outlook.com>
2025-03-19 12:42:44 +0200

    wip

--- package-reader/src/lib.rs
+++ package-reader/src/lib.rs
@@ -2,18 +2,14 @@ mod packet;
 pub mod packet_reader {
     use std::process;
 
-    use pnet::datalink::{self, NetworkInterface};
     use pnet::datalink::Channel::Ethernet;
-    use pnet::packet::ethernet::{self, EtherTypes, EthernetPacket, MutableEthernetPacket};
+    use pnet::datalink::{self};
+    use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
     use pnet::packet::ip::IpNextHeaderProtocols;
     use pnet::packet::ipv4::Ipv4Packet;
     use pnet::packet::tcp::TcpPacket;
     use pnet::packet::udp::UdpPacket;
     use pnet::packet::Packet;
-
-    pub struct Connection {
-        interface: NetworkInterface
-    }
 
     pub fn read(interface_name: String) {
         let interfaces = datalink::interfaces();
@@ -30,19 +26,25 @@ pub mod packet_reader {
         let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
             Ok(Ethernet(tx, rx)) => (tx, rx),
             Ok(_) => panic!("Unhandled channel type"),
-            Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
+            Err(e) => panic!(
+                "An error occurred when creating the datalink channel: {}",
+                e
+            ),
         };
 
-        match rx.next() {
-            Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-                println!("Source MAC: {}", packet.get_source());
-                println!("Destination MAC: {}", packet.get_destination());
+        loop {
+            match rx.next() {
+                Ok(packet) => {
+                    if let Some(packet) = EthernetPacket::new(packet) {
+                        println!("Source MAC: {}", packet.get_source());
+                        println!("Destination MAC: {}", packet.get_destination());
 
-                match_packet(&packet);
-            }
-            Err(e) => {
-                eprintln!("An erro occured while reading from the channel: {}", e);
+                        match_packet(&packet);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("An erro occured while reading from the channel: {}", e);
+                }
             }
         }
     }
@@ -51,26 +53,36 @@ pub mod packet_reader {
         match packet.get_ethertype() {
             EtherTypes::Ipv4 => {
                 if let Some(ipv4) = Ipv4Packet::new(packet.payload()) {
-                    println!("IPv4 Packet: {} -> {}", ipv4.get_source(), ipv4.get_destination());
+                    println!(
+                        "IPv4 Packet: {} -> {}",
+                        ipv4.get_source(),
+                        ipv4.get_destination()
+                    );
 
                     match ipv4.get_next_level_protocol() {
                         IpNextHeaderProtocols::Tcp => {
                             if let Some(tcp) = TcpPacket::new(ipv4.payload()) {
-                                println!("TCP Packet: {} -> {}", tcp.get_source(), tcp.get_destination());
+                                println!(
+                                    "TCP Packet: {} -> {}",
+                                    tcp.get_source(),
+                                    tcp.get_destination()
+                                );
                             }
-                        },
+                        }
                         IpNextHeaderProtocols::Udp => {
                             if let Some(udp) = UdpPacket::new(ipv4.payload()) {
-                                println!("UDP Packet: {} -> {}", udp.get_source(), udp.get_destination());
+                                println!(
+                                    "UDP Packet: {} -> {}",
+                                    udp.get_source(),
+                                    udp.get_destination()
+                                );
                             }
-                        },
+                        }
                         protocol => println!("Other protocol: {:?}", protocol),
                     }
                 }
-            },
+            }
             ethertype => println!("Other ethertype: {:?}", ethertype),
         }
-
     }
 }
-
